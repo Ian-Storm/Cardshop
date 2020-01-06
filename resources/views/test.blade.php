22dc73353884ef0ed18dfcdbd812e7bd44290f69
Ian Storm <99039737@mydavinci.nl>
2020-01-06 22:14:01 +0100

    style fix

---
+++ resources/views/test.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Cardshop Ian</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+<!--[if lte IE 6]><link rel="stylesheet" href="css/ie6.css" type="text/css" media="all" /><![endif]-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+<body>
+<!-- Shell -->
+<div class="shell">
+  <!-- Header -->
+  <div id="header">
+    <h1 id="logo"><a href="#">Cardshop Ian</a></h1>
+    <!-- Cart -->
+    <div id="cart"> <a href="#" class="cart-link">Your Shopping Cart</a>
+      <div class="cl">&nbsp;</div>
+      <span>Articles: <strong>0</strong></span> &nbsp;&nbsp; <span>Cost: <strong>€00.00</strong></span> </div>
+    <!-- End Cart -->
+    <!-- Navigation -->
+    <div id="navigation">
+      <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#">My Account</a></li>
+      </ul>
+    </div>
+    <!-- End Navigation -->
+  </div>
+  <!-- End Header -->
+  <!-- Main -->
+  <div id="main">
+    <div class="cl">&nbsp;</div>
+    <!-- Content -->
+    <div id="content">
+      <!-- Products -->
+      <div class="products">
+        <div class="cl">&nbsp;</div>
+        <ul>
+          <li> <a href="#"><img src="{{ asset('images/big1.jpg') }}" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li> <a href="#"><img src="{{ asset('images/big1.jpg') }}" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+          <li class="last"> <a href="#"><img src="{{ asset('images/big1.jpg') }}" alt="" /></a>
+            <div class="product-info">
+              <h3>LOREM IPSUM</h3>
+              <div class="product-desc">
+                <h4>WOMEN’S</h4>
+                <p>Lorem ipsum dolor sit<br />
+                  amet</p>
+                <strong class="price">$58.99</strong> </div>
+            </div>
+          </li>
+        </ul>
+        <div class="cl">&nbsp;</div>
+      </div>
+      <!-- End Products -->
+    </div>
+    <!-- End Content -->
+    <!-- Sidebar -->
+    <div id="sidebar">
+      <!-- Categories -->
+      <div class="box categories">
+        <h2>Categories <span></span></h2>
+        <div class="box-content">
+          <ul>
+            <li><a href="#">Category 1</a></li>
+            <li><a href="#">Category 2</a></li>
+            <li><a href="#">Category 3</a></li>
+            <li><a href="#">Category 4</a></li>
+            <li><a href="#">Category 5</a></li>
+            <li><a href="#">Category 6</a></li>
+            <li><a href="#">Category 7</a></li>
+            <li><a href="#">Category 8</a></li>
+            <li><a href="#">Category 9</a></li>
+            <li class="last"><a href="#">Category 10</a></li>
+          </ul>
+        </div>
+      </div>
+      <!-- End Categories -->
+    </div>
+    <!-- End Sidebar -->
+    <div class="cl">&nbsp;</div>
+  </div>
+  <!-- End Main -->
+  <!-- Side Full -->
+  <div class="side-full">
+    
+  </div>
+  <!-- End Side Full -->
+  <!-- Footer -->
+  <div id="footer">
+    <p class="left"> <a href="#">Home</a> <span>|</span> <a href="#">My Account</a> <span>|</span> <a href="https://www.youtube.com/channel/UCTVyzYP9_fkc_KxrezbIF-A">Contact</a> <p class="right">  @Adrelian </p>
+    
+  </div>
+  <!-- End Footer -->
+</div>
+<!-- End Shell -->
+</body>
+</html>
