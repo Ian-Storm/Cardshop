16061380439bb9feee75601519c3dbb1cadef5fa
Ian Storm <99039737@mydavinci.nl>
2020-02-14 23:29:47 +0100

    getting the orders for the specific person who is logged in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Models\Order;
+use App\Http\Models\OrderDetail;
+use Auth;
 
 class HomeController extends Controller
 {
@@ -17,12 +20,26 @@ class HomeController extends Controller
     }
 
     /**
-     * Show the application dashboard.
+     * Show the application dashboard and the orders.
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
     {
-        return view('home');
+        $orders = Order::where('user_id', Auth::id())->get()->toArray(); //gets the orders of the logged in user
+        $orders = array_reverse($orders); //reverses the order of the orders
+
+        for ($i=0; $i < count($orders); $i++) { 
+            $orders[$i]['products'] = []; //makes an empty array in the order
+            $orders[$i]['totalPrice'] = 0; //makes an float in the order
+            $items = OrderDetail::where('order_id', $orders[$i]['id'])->get()->toArray(); //gets all the product from the specific order
+            for ($p=0; $p < count($items); $p++) { 
+                $item = json_decode($items[$p]['orderString'], true); //makes an object from the json string
+                $item['product']['amount'] = $item['amount']; //stores amount in the product
+                $orders[$i]['totalPrice'] += (float)$item['product']['price'] * (float)$item['amount']; //makes total price by doing price x amount for each order
+                array_push($orders[$i]['products'], $item['product']); //adds a product to a products list of an order
+            }
+        }
+        return view('home', compact('orders'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,45 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Orders</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
+                    @else
+                        @foreach($orders as $order)
+                            <div class="col-md-12">
+                                <div class="card">
+                                    <div class="card-header">
+                                        Id Order {{ $order['id'] }}
+                                        <span style="float:right;">
+                                        Total: &euro;{{ $order['totalPrice'] }},-
+                                        </span>
+                                    </div>
+                                    @foreach($order['products'] as $data)
+                                    <div class="card-body">
+                                        <p class="description">
+                                            {{ $data['name'] }}
+                                        </p>
+                                        <p class="description">
+                                            {{ $data['set'] }}
+                                        </p>
+                                        <p class="description">
+                                            {{ $data['description'] }}
+                                        </p>
+                                        <p class="description">
+                                            Amount: {{ $data['amount'] }}
+                                        </p>
+                                    </div>
+                                @endforeach
+                                </div>
+                            </div>
+                        @endforeach
                     @endif
-
+                    
+                    @if (count($orders) <= 0)
                     You have no orders yet!
+                    @endif
                 </div>
             </div>
         </div>
